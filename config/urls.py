952194b752388764a06b9830945e673674c01072
Oscar Valencia <oscarvalenciaf2002@gmail.com>
2026-09-07 11:52:26 -0300

    Habilitar panel de admin en urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
 from panol.views import login_view, logout_view
 
 urlpatterns = [
+    path("admin/", admin.site.urls),  # <--- Agrega esta línea aquí
     path("", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("", include("panol.urls")),
